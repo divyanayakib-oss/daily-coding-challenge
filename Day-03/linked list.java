56f313506e987f7e6e8a4587896178852629d80a
Divanayagi B <divyanayakib@gmail.com>
2026-07-14 07:10:05 +0530

    Day-03 Added Linked list solution

---
+++ Day-03/linked list.java
@@ -0,0 +1,17 @@
+public class Linkedlist {
+    public boolean linkedlist(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if (fast == slow) {
+                return true;
+            }
+        }
+
+        return false;        
+    }
+}
