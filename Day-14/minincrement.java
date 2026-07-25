6240aaf2bea224bcfb109b1c20ece8f4a26fd634
Divanayagi B <divyanayakib@gmail.com>
2026-07-25 17:43:39 +0530

    Added Day-14 minincrement solution

---
+++ Day-14/minincrement.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minIncrementForUnique(int[] nums) {
+         Arrays.sort(nums);
+
+        int numTracker = 0; // Tracks the next unique number that should be set.
+        int minIncrement = 0; // Counts the total increments required.
+
+        for (int num : nums) {
+            numTracker = Math.max(numTracker, num);
+            minIncrement += numTracker - num;
+            numTracker += 1; // Increment the tracker for the next number.
+        }
+
+        return minIncrement;
+    }
+}
