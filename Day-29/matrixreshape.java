ce833020e4f1435b10886af4bf004a84bc507c86
Divanayagi B <divyanayakib@gmail.com>
2026-08-09 23:17:59 +0530

    Added Day-29 matrixreshape solution

---
+++ Day-29/matrixreshape.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[][] matrixReshape(int[][] mat, int r, int c) {
+         int m = mat.length;
+        int n = mat[0].length;
+        int total = m * n;
+
+        if (r * c != total) {
+            return mat;
+        }
+
+        int[][] res = new int[r][c];
+
+        for (int i = 0; i < total; i++) {
+            res[i / c][i % c] = mat[i / n][i % n];
+        }
+
+        return res;
+    }
+}
